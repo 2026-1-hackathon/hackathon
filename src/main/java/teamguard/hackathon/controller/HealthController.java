858b651002429e9484181bc4414648507e1d18aa
최유경 <122093721+YugyeongChoi@users.noreply.github.com>
2026-06-24 14:19:12 +0900

    fix: configure frontend CORS

--- src/main/java/teamguard/hackathon/controller/HealthController.java
+++ src/main/java/teamguard/hackathon/controller/HealthController.java
@@ -2,10 +2,15 @@ package teamguard.hackathon.controller;
 
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "https://hackathon-sandy-omega.vercel.app"
+})
 @RestController
 @RequestMapping("/api")
 public class HealthController {
